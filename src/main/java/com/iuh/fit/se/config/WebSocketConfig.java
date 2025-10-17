3a3cb5a752c44a78664e890835e0c159834f5f86
MrTroll28 <{nguyenoke123@gmail.com}>
2025-10-17 17:00:55 +0700

    create WebSocketConfig.java

---
+++ src/main/java/com/iuh/fit/se/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.iuh.fit.se.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Endpoint cho client kết nối
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Prefix cho topic và queue
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
