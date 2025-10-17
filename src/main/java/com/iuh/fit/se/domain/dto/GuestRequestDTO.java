86a0a18eaaca7bc979eb641663ef326f24b11e7b
MrTroll28 <{nguyenoke123@gmail.com}>
2025-10-18 05:33:32 +0700

    create GuestRequestDTO.java

---
+++ src/main/java/com/iuh/fit/se/domain/dto/GuestRequestDTO.java
@@ -0,0 +1,11 @@
+package com.iuh.fit.se.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GuestRequestDTO {
+    private String fullName;
+    private String phoneNumber;
+}
