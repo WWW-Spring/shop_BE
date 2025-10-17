bac6979d34a01fcef093394f3cb5ca6861357f6d
nghia1501 <nghianguyen15012004@gmail.com>
2025-10-17 20:27:42 +0700

    add crud interface for product service

--- src/main/java/com/iuh/fit/se/service/ProductService.java
+++ src/main/java/com/iuh/fit/se/service/ProductService.java
@@ -8,4 +8,10 @@ import java.util.List;
 
 public interface ProductService {
 
+    ProductDTO createProduct(ProductDTO productDTO);
+    ProductDTO updateProduct(Long id, ProductDTO productDTO);
+    void deleteProduct(Long id);
+    ProductDTO getProductById(Long id);
+    List<ProductDTO> getAllProducts();
+
 }
